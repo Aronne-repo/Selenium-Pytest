1319106dcb21f7226885631032827f06a1a0c3aa
Aronne <285704982+Aronne-repo@users.noreply.github.com>
2026-05-20 12:19:48 +0200

    Added sidebar search test

--- pages/home_page.py
+++ pages/home_page.py
@@ -8,6 +8,9 @@ class HomePage(BasePage):
     USER_NAME = (By.CLASS_NAME, "oxd-userdropdown-name")
     USER_DROPDOWN_ARROW = (By.CLASS_NAME, "oxd-userdropdown-icon")
     USER_DROPDOWN_OPTIONS = (By.XPATH, "//ul[@class='oxd-dropdown-menu' and @role='menu']//a[@role='menuitem']")
+    SIDEBAR_CHEVRON = (By.XPATH, "//i[contains(@class,'bi-chevron-')]")
+    SIDEBAR_SEARCH_BAR = (By.CLASS_NAME, "oxd-input")
+    SIDEBAR_OPTIONS = (By.XPATH, "//li[@class='oxd-main-menu-item-wrapper']")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -27,3 +30,22 @@ class HomePage(BasePage):
     def get_user_dropdown_options(self):
         dropdown_options = self.get_elements(self.USER_DROPDOWN_OPTIONS)
         return [option.text for option in dropdown_options]
+
+    def open_sidebar_chevron(self):
+        element = self.get_element(self.SIDEBAR_CHEVRON)
+        classes = element.get_attribute("class").split()
+        if "toggled" in classes:
+            self.click(element.find_element(By.XPATH, "./parent::button"))
+
+    def close_sidebar_chevron(self):
+        element = self.get_element(self.SIDEBAR_CHEVRON)
+        classes = element.get_attribute("class").split()
+        if "toggled" not in classes:
+            self.click(element.find_element(By.XPATH, "./parent::button"))
+
+    def search_in_sidebar(self, text):
+        self.send_keys(self.SIDEBAR_SEARCH_BAR, text)
+
+    def get_sidebar_search_results(self):
+        sidebar_options = self.get_elements(self.SIDEBAR_OPTIONS)
+        return [option.text for option in sidebar_options]
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -9,3 +9,9 @@ def test_user_dropdown(logged_driver):
     page = HomePage(logged_driver)
     page.open_user_dropdown()
     assert page.get_user_dropdown_options() == ["About", "Support", "Change Password", "Logout"]
+
+def test_sidebar_search(logged_driver):
+    page = HomePage(logged_driver)
+    page.open_sidebar_chevron()
+    page.search_in_sidebar("My Info")
+    assert "My Info" in page.get_sidebar_search_results()
